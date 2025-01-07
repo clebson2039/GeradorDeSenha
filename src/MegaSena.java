9941b2ab4fd7ad9b26b4a9a7c55529afebb2c6e8
clebson2039 <clebsonfarias1@gmail.com>
2025-01-07 14:20:47 -0300

    Implatação da classe Scanner

--- src/MegaSena.java
+++ src/MegaSena.java
@@ -1,15 +1,23 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class MegaSena {
     public static void main(String[] args) throws Exception {
-        Random generate = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Olá " + name);
+        /*Random generate = new Random();
 
         for(int i = 0; i < 6; i++){
             int number = generate.nextInt(60);
             System.out.println(number);
         }
 
-        /*int i = 0;
+        int i = 0;
         while (i < 6) {
             int number = generate.nextInt(60);
             System.out.println(number);
